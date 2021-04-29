4439e8bde3ce34c79fde234221d1828cdfdd89a7
William Derek Dias <williamdkdevops@gmail.com>
2021-04-29 20:20:06 -0300

    Implementation of GET method validator returns listing people

--- src/test/java/br/com/supernova/persons/controller/PersonControllerTest.java
+++ src/test/java/br/com/supernova/persons/controller/PersonControllerTest.java
@@ -4,6 +4,7 @@ import br.com.supernova.persons.builder.MessageBuilder;
 import br.com.supernova.persons.builder.PersonBuilder;
 import br.com.supernova.persons.dto.request.PersonDTO;
 import br.com.supernova.persons.dto.response.MessageResponseDTO;
+import br.com.supernova.persons.exceptions.PersonNotFoundException;
 import br.com.supernova.persons.service.PersonService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,6 +18,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
+
+import java.util.Collections;
+import java.util.List;
 
 import static br.com.supernova.persons.builder.PersonBuilder.jsonToString;
 import static org.mockito.Mockito.when;
@@ -76,5 +80,30 @@ public class PersonControllerTest {
                 .andExpect(jsonPath("$.id", is(personDTO.getId())))
                 .andExpect(jsonPath("$.firstName", is(personDTO.getFirstName())))
                 .andExpect(jsonPath("$.lastName", is(personDTO.getLastName())));
+    }
+
+    @Test
+    void testWhenGETWithInvalidIsCalledThenAnErrorMessageShouldBeReturned() throws Exception {
+        final Long INVALID_ID = 2L;
+
+        when(service.findByPerson(INVALID_ID)).thenThrow(PersonNotFoundException.class);
+
+        mockMvc.perform(get(URL_PATH + "/" + INVALID_ID)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testWhenGETIsCalledThenPeopleListShouldBeReturned() throws Exception {
+        PersonDTO personDTO = PersonBuilder.builder().build().toPersonDTO();
+        List<PersonDTO> personDTOList = Collections.singletonList(personDTO);
+
+        when(service.returnAllPeople()).thenReturn(personDTOList);
+
+        mockMvc.perform(get(URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].firstName", is(personDTO.getFirstName())))
+                .andExpect(jsonPath("$[0].lastName", is(personDTO.getLastName())));
     }
 }
