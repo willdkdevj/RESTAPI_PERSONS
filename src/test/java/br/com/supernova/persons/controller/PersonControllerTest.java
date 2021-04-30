3ebf4d20b350cd394a1d3b76dcf70647e7a15be7
William Derek Dias <williamdkdevops@gmail.com>
2021-04-29 21:08:21 -0300

    Implementation of DELETE method validator

--- src/test/java/br/com/supernova/persons/controller/PersonControllerTest.java
+++ src/test/java/br/com/supernova/persons/controller/PersonControllerTest.java
@@ -1,9 +1,10 @@
 package br.com.supernova.persons.controller;
 
-import br.com.supernova.persons.builder.MessageBuilder;
 import br.com.supernova.persons.builder.PersonBuilder;
 import br.com.supernova.persons.dto.request.PersonDTO;
+import br.com.supernova.persons.dto.request.PhoneDTO;
 import br.com.supernova.persons.dto.response.MessageResponseDTO;
+import br.com.supernova.persons.enums.PhoneType;
 import br.com.supernova.persons.exceptions.PersonNotFoundException;
 import br.com.supernova.persons.service.PersonService;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,10 +19,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
-
 import java.util.Collections;
 import java.util.List;
 
+import static br.com.supernova.persons.builder.MessageBuilder.*;
 import static br.com.supernova.persons.builder.PersonBuilder.jsonToString;
 import static org.mockito.Mockito.when;
 import static org.hamcrest.core.Is.is;
@@ -57,7 +58,7 @@ public class PersonControllerTest {
     @Test
     void testWhenPOSTIsCalledThenAPersonShouldBeCreated() throws Exception {
         PersonDTO personDTO = PersonBuilder.builder().build().toPersonDTO();
-        MessageResponseDTO messageResponseDTO = MessageBuilder.createInspectMessageResponse(personDTO.getId());
+        MessageResponseDTO messageResponseDTO = createInspectMessageResponse(personDTO.getId());
 
         when(service.registerPerson(personDTO)).thenReturn(messageResponseDTO);
 
@@ -105,5 +106,36 @@ public class PersonControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].firstName", is(personDTO.getFirstName())))
                 .andExpect(jsonPath("$[0].lastName", is(personDTO.getLastName())));
+    }
+
+    @Test
+    void testWhenPUTIsCalledThenAPersonDTOShouldBeUpdated() throws Exception {
+        var updatePersonDTOID = 1L;
+        PersonDTO personDTO = PersonBuilder.builder().build().toPersonDTO();
+        MessageResponseDTO messageResponseDTO = updateMessageResponse("Person successfully updated with ID", 1L);
+
+        when(service.updateById(updatePersonDTOID, personDTO))
+            .thenReturn(messageResponseDTO);
+
+        mockMvc.perform(put(URL_PATH + "/" + updatePersonDTOID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonToString(personDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message", is(messageResponseDTO.getMessage())));
+    }
+
+    @Test
+    void testWhenDELETEIsCalledThenAPersonShouldBeDeleted() throws Exception {
+        final Long INSPECT_PERSON_ID = 1L;
+
+        mockMvc.perform(delete(URL_PATH + "/" + INSPECT_PERSON_ID)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
+
+    private MessageResponseDTO updateMessageResponse(String message, Long id) {
+        return MessageResponseDTO.builder()
+                .message(message + id)
+                .build();
     }
 }
